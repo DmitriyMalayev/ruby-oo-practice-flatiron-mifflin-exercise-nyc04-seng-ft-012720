1d0e99b88dcc5c2a94f55b70505669465073b1d0
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-21 01:53:55 +0000

    Automatically backed up by Learn

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,25 @@
 class Employee
+    attr_reader :name, :salary, :manager_name
+
+    @@all = []
+    def initialize(name, salary, manager_name)
+        @name = name
+        @salary = salary
+        @manager_name = manager_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+    def self.paid_over(fix)
+        @@all.select do |salary|
+        salary > fix
+
+        end
+        return
+    end
 
 end
