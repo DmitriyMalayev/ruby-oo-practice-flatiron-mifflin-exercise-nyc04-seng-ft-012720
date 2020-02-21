7a9725b55aa53e58eb61f838673ffcad3135a69e
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-21 01:53:50 +0000

    Automatically backed up by Learn

--- run.rb
+++ run.rb
@@ -1,10 +1,10 @@
 require_relative "lib/Manager"
 require_relative "lib/Employee"
 
-dmitriy = Employee.new("dmitriy", 100000, "annie") 
-firuz = Employee.new("firuz", 100000, "jack")  
+dmitriy = Employee.new("dmitriy", 100000, "annie")
+firuz = Employee.new("firuz", 100000, "jack")
 
 annie = Manager.new("annie", "engineering", 20, dmitriy)
-jack = Manager.new("jack", "management", 21, firuz) 
+jack = Manager.new("jack", "management", 21, firuz)
 
 puts Employee.paid_over(100) 
