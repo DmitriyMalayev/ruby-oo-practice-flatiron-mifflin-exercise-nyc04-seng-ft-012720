d7872ca95a17d99b5818031e95ac4d6774263ae5
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-21 01:53:59 +0000

    Automatically backed up by Learn

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,13 @@
 class Manager
+    attr_reader :name, :department, :age
+    attr_accessor :worker
+    @@all = []
+
+    def initialize(name, department, age, worker)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+    end
 
 end
